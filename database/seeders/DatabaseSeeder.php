5eeb529039572228b84c1ded950acf0e75d357a7
Muhammad waseem <waseemrajpoot3033@gmail.com>
2024-11-03 20:36:02 +0500

    changes for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'admin',
-            'email' => 'shoaibchohan200@gmail.com',
-            'password' => Hash::make('shoaibchohan200'),
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
         ]);
 
         $this->call([
