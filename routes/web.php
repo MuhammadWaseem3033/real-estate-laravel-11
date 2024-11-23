9cb6d471b871baf4ad87df79b6fc8e6283ba04e9
Muhammad waseem <waseemrajpoot3033@gmail.com>
2024-11-23 16:23:35 +0500

    ft

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-require __DIR__ . '/frontend.php';
+Route::get('/', function () {
+    return view('welcome');
+});
+// require __DIR__ . '/frontend.php';
 
 // https://www.youtube.com/watch?v=w2FAlJxJtq8
 // https://www.youtube.com/watch?v=dpJDV25tptw
